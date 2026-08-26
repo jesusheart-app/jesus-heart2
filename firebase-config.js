920dc9be4378e83f4ddb8576b06f7fcba467278e
jesusheart-app <mesuk80@gmail.com>
2026-08-26 14:46:00 +0900

    Add Firebase web configuration

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBLTQFMgzcG8ieJ5DGpbISb5q6yIfmxi5M",
+  authDomain: "jesus-heart2.firebaseapp.com",
+  projectId: "jesus-heart2",
+  storageBucket: "jesus-heart2.firebasestorage.app",
+  messagingSenderId: "193075501677",
+  appId: "1:193075501677:web:45f5879fee29d2ad1bfd64"
+};
