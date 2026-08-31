e364c24cfe1ae9c40c3177fdaaf51d6526050e64
jesusheart-app <mesuk80@gmail.com>
2026-08-31 20:46:23 +0900

    웹 푸시 공개 키 설정

--- app-settings.js
+++ app-settings.js
@@ -2,5 +2,6 @@ export const appSettings = Object.freeze({
   churchJoinCodeHash:
     "56e766634bc510368a1a3deb083b96763b301054c7b78f0273db4933a980ac41",
   // Firebase 콘솔 > 프로젝트 설정 > Cloud Messaging > 웹 푸시 인증서의 공개 키
-  firebaseWebPushPublicKey: ""
+  firebaseWebPushPublicKey:
+    "BKgFOGzI1Ffj71MITvzfKDIQI2o9jBVWFtFQrx5SD6tGfHNoxdPORfKYkxd_9f5S1ps_GWUyiS0InP9NuMGhbmU"
 });
