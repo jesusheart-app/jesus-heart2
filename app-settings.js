18eee4ffc0102a4f4d3ef6af6b53b0a261b69d75
jesusheart-app <mesuk80@gmail.com>
2026-08-31 19:07:22 +0900

    매일 말씀 알림 자동 발송

--- app-settings.js
+++ app-settings.js
@@ -1,4 +1,6 @@
 export const appSettings = Object.freeze({
   churchJoinCodeHash:
-    "56e766634bc510368a1a3deb083b96763b301054c7b78f0273db4933a980ac41"
+    "56e766634bc510368a1a3deb083b96763b301054c7b78f0273db4933a980ac41",
+  // Firebase 콘솔 > 프로젝트 설정 > Cloud Messaging > 웹 푸시 인증서의 공개 키
+  firebaseWebPushPublicKey: ""
 });
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/12.18.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.18.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBLTQFMgzcG8ieJ5DGpbISb5q6yIfmxi5M",
+  authDomain: "jesus-heart2.firebaseapp.com",
+  projectId: "jesus-heart2",
+  storageBucket: "jesus-heart2.firebasestorage.app",
+  messagingSenderId: "193075501677",
+  appId: "1:193075501677:web:45f5879fee29d2ad1bfd64"
+});
+
+firebase.messaging();
